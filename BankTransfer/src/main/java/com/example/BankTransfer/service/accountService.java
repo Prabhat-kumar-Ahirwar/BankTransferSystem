30483b6b8a207f03478aefc748317d13e3028f7d
Prabhat-kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-03-27 19:02:07 +0530

    Transcation

--- BankTransfer/src/main/java/com/example/BankTransfer/service/accountService.java
+++ BankTransfer/src/main/java/com/example/BankTransfer/service/accountService.java
@@ -1,4 +1,33 @@
 package com.example.BankTransfer.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.BankTransfer.entity.account;
+import com.example.BankTransfer.repository.accountRepo;
+
+@Service
 public class accountService {
+
+    @Autowired
+    private accountRepo repo;
+
+    @Transactional
+    public void transfer(Long fromId, Long toId, double amount) {
+
+        account from = repo.findById(fromId).orElseThrow();
+        account to = repo.findById(toId).orElseThrow();
+
+        // deduct
+        from.setBalance(from.getBalance() - amount);
+        repo.save(from);
+
+        if (true) {
+            throw new RuntimeException("Transaction Failed!");
+        }
+
+        // add
+        to.setBalance(to.getBalance() + amount);
+        repo.save(to);
+    }
 }
