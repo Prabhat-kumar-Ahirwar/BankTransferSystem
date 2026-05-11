bd15adc47de12f51b8c747cdb716fe1896a02528
Prabhat-Kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-05-11 18:30:12 +0530

    Re- Create

--- BankTransfer/src/main/java/com/example/BankTransfer/repository/accountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.BankTransfer.repository;
-
-import com.example.BankTransfer.entity.account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface accountRepo extends JpaRepository<account,Long> {
-}
