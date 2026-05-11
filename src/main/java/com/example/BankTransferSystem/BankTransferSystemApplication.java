f1f207735a3e426bb9e8f0727a8d7661a3205d42
Prabhat-Kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-05-11 18:26:30 +0530

    init

---
+++ src/main/java/com/example/BankTransferSystem/BankTransferSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankTransferSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransferSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransferSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BankTransferSystem/BankTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BankTransferSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
